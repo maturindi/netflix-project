00f1c034fbb3c056eb1979d665a090fdad2104b0
Maturin <maturindi@gmail.com>
2022-09-12 13:32:16 -0400

    modified per need

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,12 @@
 # code to create ec2
 # type --> t2.micro
 # OS --> linux
+
+resource "aws_instance" "test" {
+  ami           = "ami-05fa00d4c63e32376"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "test"
+    Createdby = "Maturin"
+  }
+}
